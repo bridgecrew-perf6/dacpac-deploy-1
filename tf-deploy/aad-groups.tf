da88eb6c9c964d1f3eaa24df0ce3ef4ae3bfc549
v-fmolyneux <v-fmolyneux@microsoft.com>
2022-01-17 15:30:14 +0000

    Renaming database

--- tf-deploy/aad-groups.tf
+++ tf-deploy/aad-groups.tf
@@ -4,14 +4,14 @@ resource "azuread_group" "sql_server_admins" {
   security_enabled = true
 }
 
-resource "azuread_group" "sql_mctest_database_readers_group" {
-  display_name     = local.mctest_readers_group_name
+resource "azuread_group" "sql_sampledb_database_readers_group" {
+  display_name     = local.sampledb_readers_group_name
   owners           = [data.azuread_client_config.current.object_id]
   security_enabled = true
 }
 
-resource "azuread_group" "sql_mctest_database_writers_group" {
-  display_name     = local.mctest_writers_group_name
+resource "azuread_group" "sql_sampledb_database_writers_group" {
+  display_name     = local.sampledb_writers_group_name
   owners           = [data.azuread_client_config.current.object_id]
   security_enabled = true
 }
--- tf-deploy/locals.tf
+++ tf-deploy/locals.tf
@@ -7,10 +7,10 @@ locals {
   sql_server_admin_username = format("%s-admin", local.sql_server_name)
 
   // SQL Database
-  sql_database_name = "McTest"
+  sql_database_name = "sampledb"
 
   // AAD Group Names
   sql_server_admin_group_name = format("sg-ag-sql-%s-%s-%s-admins", var.workload, var.env, var.instance)
-  mctest_readers_group_name   = format("sg-ag-sql-%s-%s-%s-mctest-readers", var.workload, var.env, var.instance)
-  mctest_writers_group_name   = format("sg-ag-sql-%s-%s-%s-mctest-readers", var.workload, var.env, var.instance)
+  sampledb_readers_group_name   = format("sg-ag-sql-%s-%s-%s-sampledb-readers", var.workload, var.env, var.instance)
+  sampledb_writers_group_name   = format("sg-ag-sql-%s-%s-%s-sampledb-readers", var.workload, var.env, var.instance)
 }
